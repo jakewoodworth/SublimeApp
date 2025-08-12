bc5bbca978862621b64e84f331174e92c9281ad0
jakewoodworth <jakespy7@gmail.com>
2025-08-12 15:11:53 -0400

    Add Vite React plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import path from 'path';
+import react from '@vitejs/plugin-react';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      plugins: [react()],
       define: {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
